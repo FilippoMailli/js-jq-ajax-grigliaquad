be80adbda88d64c65bc198dd1e9eed8eae4b66c1
Filippo Mailli <filippo.mailli@gmail.com>
2020-03-16 16:16:10 +0100

    .3 Miglioramento con Handlebars

--- js/main.js
+++ js/main.js
@@ -6,14 +6,26 @@ BONUS: OTTIMIZZARE CON HANDLEBARS
 */
 
 $(document).ready(function() {
+
+
+    //HANDLEBARS
+
+    var source = $('#square-template').html();
+    var template = Handlebars.compile(source);
+
+    for(i = 0; i < 36; i++) {
+        $('.container').append(source);
+    }
+
+
     $(".square").click(function(){
-        var squareAttivo = $(this);
+        var squareAttivo = $(this);                                         //Do nome a il riquadro cliccato così da poterlo riusare
         $.ajax({
             url: 'https://flynn.boolean.careers/exercises/api/random/int',  //Funzione base per generare numeri random da 0 a 9
             method: 'GET',
             success: function (data) {
                 var numeroRandom = data.response;
-                if(numeroRandom >= 5){
+                if(numeroRandom >= 5){                                      //Funzione if che se sopra o pari a 5 richiamo il giallo, sotto il verde
                     diventaGiallo(squareAttivo, numeroRandom);
                 } else {
                     diventaVerde(squareAttivo, numeroRandom);
@@ -26,9 +38,9 @@ $(document).ready(function() {
     });
 
     function diventaGiallo(quadratoAttivo, numero){
-        quadratoAttivo.empty();
-        quadratoAttivo.css("background-color", "yellow");
-        quadratoAttivo.append(numero);
+        quadratoAttivo.empty();                                     //Pulizia del vecchio quadrato (in caso ricliccassi)
+        quadratoAttivo.css("background-color", "yellow");           //Do il background giallo con css
+        quadratoAttivo.append(numero);                              //Inserisco il numero all'interno del quadrato attivo
     }
 
 
@@ -37,6 +49,9 @@ $(document).ready(function() {
         quadratoAttivo.css("background-color", "green");
         quadratoAttivo.append(numero);
     };
+
+
+
 
 
 });
